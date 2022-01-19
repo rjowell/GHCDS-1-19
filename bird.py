a3c1d890f4da3742a2cafde3339b797ae9fd671a
rjowell20007 <44178512+rjowell@users.noreply.github.com>
2022-01-19 13:44:03 +0000

    Initial commit

---
+++ bird.py
@@ -0,0 +1,39 @@
+'''
+BIRD
+-Color.
+-Beak Type
+-Species
+-Wing Size
+-Bird's Size
+-Is it a carnivorous bird or herbivore
+-Can the bird fly
+-Can they tweet
+-has long legs
+'''
+
+class Bird:
+
+  def fly(self):
+    print("Up Up and away!")
+
+  def tweet(self):
+    print("Tweet Tweet")
+  
+  #COMMENTS
+  #fly
+  #eat
+  #look for food
+  #brith chicks
+  #call
+  
+  
+  def __init__(self,_color,_species,_wingSize,_isCarnivore,_canFly,_canTweet):
+    self.wingSize = _wingSize
+    self.color = _color
+    self.species = _species
+    self.carnivore = _isCarnivore
+    self.canFly = _canFly
+    self.canTweet = _canTweet
+
+  def eat(self):
+    print("Yummy!")
---
+++ main.py
@@ -0,0 +1,8 @@
+from bird import Bird
+
+#  def __init__(self,_color,_species,_wingSize,_isCarnivore,_canFly,_canTweet):
+
+myBird = Bird("yellow","Ostrich",20,True,False,True)
+
+print(myBird.carnivore)
+print(myBird.wingSize)
